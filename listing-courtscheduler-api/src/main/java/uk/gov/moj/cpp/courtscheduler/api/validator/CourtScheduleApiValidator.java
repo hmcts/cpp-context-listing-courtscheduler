5f47abb89e5effdcd2cb8df398df221bc15331f6
Baris Ozturkmen <baris.ozturkmen@hmcts.net>
2024-07-02 04:41:16 +0100

    update for sonar issues

--- listing-courtscheduler-api/src/main/java/uk/gov/moj/cpp/courtscheduler/api/validator/CourtScheduleApiValidator.java
+++ listing-courtscheduler-api/src/main/java/uk/gov/moj/cpp/courtscheduler/api/validator/CourtScheduleApiValidator.java
@@ -27,8 +27,6 @@ import org.slf4j.LoggerFactory;
 
 public class CourtScheduleApiValidator {
     private static final Logger LOGGER = LoggerFactory.getLogger(CourtScheduleApiValidator.class.getName());
-    private static final String MISSING_BOTH_DURATION_AND_SLOTS = "At least one of maxSlots or maxDuration should be supplied";
-    private static final String BOTH_DURATION_AND_SLOTS_SUPPLIED = "Either maxSlots or maxDuration should be supplied";
 
     @SuppressWarnings("squid:MethodCyclomaticComplexity")
     public JsonObject getCourtSchedulesValidation(final CourtScheduleRequestParam courtScheduleRequestParam) {
