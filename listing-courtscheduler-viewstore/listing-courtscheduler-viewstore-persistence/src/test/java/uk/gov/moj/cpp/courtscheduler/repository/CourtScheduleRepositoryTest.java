bf23da8cea068d1f5bc8f85a7434b9d37cce8dda
siby.george1 <siby.george1@hmcts.net>
2024-07-09 16:51:03 +0100

    DD-33669 : fix get court schedule criteria.

--- listing-courtscheduler-viewstore/listing-courtscheduler-viewstore-persistence/src/test/java/uk/gov/moj/cpp/courtscheduler/repository/CourtScheduleRepositoryTest.java
+++ listing-courtscheduler-viewstore/listing-courtscheduler-viewstore-persistence/src/test/java/uk/gov/moj/cpp/courtscheduler/repository/CourtScheduleRepositoryTest.java
@@ -1,35 +1,5 @@
 package uk.gov.moj.cpp.courtscheduler.repository;
 
-import static io.github.benas.randombeans.api.EnhancedRandom.random;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
-
-import uk.gov.moj.cpp.courtscheduler.domain.AllocatedSlot;
-import uk.gov.moj.cpp.courtscheduler.domain.CourtScheduleRequestParam;
-import uk.gov.moj.cpp.courtscheduler.domain.HearingSlotRequestParam;
-import uk.gov.moj.cpp.courtscheduler.domain.MiFilterCriteria;
-import uk.gov.moj.cpp.courtscheduler.domain.Result;
-import uk.gov.moj.cpp.courtscheduler.domain.utils.DateUtils;
-import uk.gov.moj.cpp.courtscheduler.persist.entity.AllocatedListing;
-import uk.gov.moj.cpp.courtscheduler.persist.entity.CourtSchedule;
-import uk.gov.moj.cpp.courtscheduler.persist.entity.CourtScheduleJudiciary;
-import uk.gov.moj.cpp.courtscheduler.persist.entity.CourtScheduleJudiciaryKey;
-import uk.gov.moj.cpp.courtscheduler.persist.entity.ProvisionalBooking;
-import uk.gov.moj.cpp.courtscheduler.persist.entity.ProvisionalBookingKey;
-
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.OffsetDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-import javax.inject.Inject;
-
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
@@ -38,6 +8,25 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import uk.gov.moj.cpp.courtscheduler.domain.*;
+import uk.gov.moj.cpp.courtscheduler.domain.utils.DateUtils;
+import uk.gov.moj.cpp.courtscheduler.persist.entity.AllocatedListing;
+import uk.gov.moj.cpp.courtscheduler.persist.entity.CourtSchedule;
+import uk.gov.moj.cpp.courtscheduler.persist.entity.CourtScheduleJudiciary;
+import uk.gov.moj.cpp.courtscheduler.persist.entity.*;
+
+import javax.inject.Inject;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static io.github.benas.randombeans.api.EnhancedRandom.random;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 
 @RunWith(CdiTestRunner.class)
 public class CourtScheduleRepositoryTest {
@@ -89,7 +78,7 @@ public class CourtScheduleRepositoryTest {
                 .withPanel(panel)
                 .build();
 
-        Result result = courtScheduleRepository.update(courtScheduleEntity,updatedCourtSchedule);
+        Result result = courtScheduleRepository.update(courtScheduleEntity, updatedCourtSchedule);
         assertThat(result.isSuccess(), is(true));
     }
 
@@ -109,16 +98,23 @@ public class CourtScheduleRepositoryTest {
     @Test
     public void shouldFindCourtSchedulesByCourtScheduleRequestParam() {
         // given
-        CourtSchedule courtSchedule = random(CourtSchedule.class);
-        courtScheduleRepository.save(courtSchedule);
-        String courtCentreId = courtSchedule.getCourtHouseId();
-        final CourtScheduleRequestParam courtScheduleRequestParam = getCourtScheduleRequestParam(courtSchedule, courtCentreId);
+        CourtSchedule matchingCourtSchedule = random(CourtSchedule.class);
+        courtScheduleRepository.save(matchingCourtSchedule);
+        // and
+        CourtSchedule unMatchingCourtSchedule = random(CourtSchedule.class);
+        unMatchingCourtSchedule.setCourtHouseId(matchingCourtSchedule.getCourtHouseId());
+        unMatchingCourtSchedule.setSessionDate(matchingCourtSchedule.getSessionDate());
+        courtScheduleRepository.save(unMatchingCourtSchedule);
+
+        String courtCentreId = matchingCourtSchedule.getCourtHouseId();
+        final CourtScheduleRequestParam courtScheduleRequestParam = getCourtScheduleRequestParam(matchingCourtSchedule, courtCentreId);
 
         // when
         List<uk.gov.moj.cpp.courtscheduler.domain.CourtSchedule> results = courtScheduleRepository.findBy(courtScheduleRequestParam);
 
         // then
-        assertThat(results.isEmpty(), is(false));
+        assertThat(results.size(), is(1));
+        assertThat(results.get(0).getCourtRoomId(), is(matchingCourtSchedule.getCourtRoomId()));
     }
 
     private static CourtScheduleRequestParam getCourtScheduleRequestParam(final CourtSchedule courtSchedule, final String courtCentreId) {
@@ -372,8 +368,8 @@ public class CourtScheduleRepositoryTest {
 
         List<uk.gov.moj.cpp.courtscheduler.domain.CourtSchedule> courtSchedules = courtScheduleRepository.deleteCourtSchedule(courtScheduleIdList);
 
-        assertNull( courtScheduleRepository.findBy(courtScheduleId1));
-        assertNull( courtScheduleRepository.findBy(courtScheduleId2));
+        assertNull(courtScheduleRepository.findBy(courtScheduleId1));
+        assertNull(courtScheduleRepository.findBy(courtScheduleId2));
         assertEquals(true, courtSchedules.isEmpty());
     }
 
