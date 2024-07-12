77a138ecaa0d47e23bcf596f5b08432dedca8956
Baris Ozturkmen <baris.ozturkmen@hmcts.net>
2024-07-12 02:20:02 +0100

    fixed available slots on update

--- listing-courtscheduler-api/src/main/java/uk/gov/moj/cpp/courtscheduler/api/service/CourtScheduleService.java
+++ listing-courtscheduler-api/src/main/java/uk/gov/moj/cpp/courtscheduler/api/service/CourtScheduleService.java
@@ -48,9 +48,7 @@ public class CourtScheduleService {
         if (isBusinessTypeChangeInvalid(updateCourtSchedule, requester, persistedBusinessType)) {
             return new Result("Business Type cannot be changed from Slot to Non-Slot and vice versa", false);
         }
-        if (maxSlotsOrDurationChanged(updateCourtSchedule, persistedCourtSchedule)) {
             updateAvailability(updateCourtSchedule, persistedCourtSchedule);
-        }
 
         String courtRoomId = updateCourtSchedule.getCourtRoomId();
 
