1eb63f40177279a5b1d3372eb7cc4d6955264032
Mehmet Alpak <mehmet.alpak2@HMCTS.NET>
2024-07-23 17:24:25 +0100

    DD-33943 ut coverage for propertiesloader class, some code refactors

--- listingcourtscheduler-api/src/main/java/uk/gov/moj/cpp/courtscheduler/api/service/rotafileprocessor/RotaFileParser.java
+++ listingcourtscheduler-api/src/main/java/uk/gov/moj/cpp/courtscheduler/api/service/rotafileprocessor/RotaFileParser.java
@@ -28,8 +28,11 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
@@ -68,9 +71,21 @@ public class RotaFileParser {
     private static final String TAG_ROTA_PERIOD = "rotaPeriod";
 
     private static final Set<String> attributeTags = new TreeSet<>(asList(TAG_MAGISTRATE, TAG_DISTRICT_JUDGE, TAG_VENUE, TAG_SCHEDULE));
-    private static final Map<String, String> requiredElements = PropertiesLoader.getXmlProperties("rotaXml.properties");
+    private Map<String, String> requiredElements = new ConcurrentHashMap<>();
+
+    @Inject
+    private PropertiesLoader propertiesLoader;
+
+    @PostConstruct
+    public void initialize() {
+        requiredElements = propertiesLoader.getXmlProperties("rotaXml.properties");
+    }
 
     public Map<RotaPayload, Map<String, Map<String, String>>> parse(final String file, final byte[] content) {
+        if (requiredElements.isEmpty()) {
+            requiredElements = propertiesLoader.getXmlProperties("rotaXml.properties");
+        }
+
         final Map<String, String> locations = new HashMap<>();
         final Map<String, String> venues = new HashMap<>();
         final Map<String, Map<String, String>> magistrates = new HashMap<>();
--- listingcourtscheduler-api/src/main/java/uk/gov/moj/cpp/courtscheduler/api/service/rotafileprocessor/util/PropertiesLoader.java
+++ listingcourtscheduler-api/src/main/java/uk/gov/moj/cpp/courtscheduler/api/service/rotafileprocessor/util/PropertiesLoader.java
@@ -1,5 +1,6 @@
 package uk.gov.moj.cpp.courtscheduler.api.service.rotafileprocessor.util;
 
+import static java.util.Objects.nonNull;
 import static java.util.stream.Collectors.toMap;
 
 import uk.gov.moj.cpp.courtscheduler.api.exception.RotaFileProcessorException;
@@ -9,22 +10,24 @@ import java.io.InputStream;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class PropertiesLoader {
 
-    private PropertiesLoader() {
-    }
+    private final Properties properties = new Properties();
 
-    public static Map<String, String> getXmlProperties(final String propertiesFile) {
-        final Properties props = new Properties();
-
-        try (final InputStream input = PropertiesLoader.class.getClassLoader().getResourceAsStream(propertiesFile)) {
-            props.load(input);
+    public Map<String, String> getXmlProperties(final String propertiesFile) {
+        try (final InputStream inputStream = PropertiesLoader.class.getClassLoader().getResourceAsStream(propertiesFile)) {
+            if (nonNull(inputStream)) {
+                properties.load(inputStream);
+            }
         } catch (final IOException e) {
             throw new RotaFileProcessorException(e);
         }
 
-        return props.stringPropertyNames()
+        return properties.stringPropertyNames()
                 .stream()
-                .collect(toMap(props::getProperty, v -> v));
+                .collect(toMap(properties::getProperty, v -> v));
     }
 }
