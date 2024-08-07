16b9e94fc005cf6b2fc3d560bb6ccdac2fba34a7
Mehmet Alpak <mehmet.alpak2@HMCTS.NET>
2024-08-07 20:49:58 +0100

    DD-34222 set courtscheduleid for existing slots at court_schedule_judiciary

--- listingcourtscheduler-api/src/main/java/uk/gov/moj/cpp/courtscheduler/api/service/rotafileprocessor/RotaFileProcessorService.java
+++ listingcourtscheduler-api/src/main/java/uk/gov/moj/cpp/courtscheduler/api/service/rotafileprocessor/RotaFileProcessorService.java
@@ -296,6 +296,7 @@ public class RotaFileProcessorService {
 
         final List<String> incomingSlotProfileIds = slots.values().stream().map(CourtSchedule::getListingProfileId).toList();
         final List<String> existingSlotProfileIds = existingSlotList.stream().map(CourtSchedule::getListingProfileId).toList();
+        final Map<String, CourtSchedule> existingSlotMap = existingSlotList.stream().collect(Collectors.toMap(CourtSchedule::getListingProfileId, courtSchedule -> courtSchedule));
 
         final List<String> existingSlotScheduleIds = existingSlotList.stream().map(CourtSchedule::getCourtScheduleId).toList();
         final List<String> existingNonMigratedSlotScheduleIds = existingSlotList.stream()
@@ -333,10 +334,14 @@ public class RotaFileProcessorService {
 
         final Collection<CourtScheduleJudiciary> newCourtScheduleJudiciaries = schedules.stream()
                 .filter(schedule -> newSlotProfileIds.contains(schedule.getCourtListingProfileId())).toList();
-        final Collection<CourtScheduleJudiciary> courtScheduleJudiciariesForMigratedExistingSlots = schedulesForMigrated
+        final Collection<CourtScheduleJudiciary> courtScheduleJudiciariesForMigratedExistingSlots = new ArrayList<>();
+        schedulesForMigrated
                 .stream()
                 .filter(courtScheduleForMigrated -> existingSlotProfileIds.contains(courtScheduleForMigrated.getCourtListingProfileId()))
-                .toList();
+                .forEach(courtScheduleJudiciary -> {
+                    courtScheduleJudiciary.setCourtScheduleId(existingSlotMap.get(courtScheduleJudiciary.getCourtListingProfileId()).getCourtScheduleId());
+                    courtScheduleJudiciariesForMigratedExistingSlots.add(courtScheduleJudiciary);
+                });
         final Map<String, List<CourtScheduleJudiciary>> relatedJudiciarySchedules = courtScheduleJudiciaryService.findRelatedJudiciarySchedules(existingSlotScheduleIds);
 
         Map<String, CourtSchedule> newSlots = slots.entrySet().stream()
