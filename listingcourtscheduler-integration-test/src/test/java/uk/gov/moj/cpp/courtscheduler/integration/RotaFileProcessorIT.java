5be54698a609f5601c76cb4855bea7eba1bf7f5b
Mehmet Alpak <mehmet.alpak2@HMCTS.NET>
2024-09-05 12:47:16 +0100

    DD-34216 fixing  rotafileprocessor it test failures

--- listingcourtscheduler-integration-test/src/test/java/uk/gov/moj/cpp/courtscheduler/integration/RotaFileProcessorIT.java
+++ listingcourtscheduler-integration-test/src/test/java/uk/gov/moj/cpp/courtscheduler/integration/RotaFileProcessorIT.java
@@ -2,11 +2,13 @@ package uk.gov.moj.cpp.courtscheduler.integration;
 
 import static java.lang.String.format;
 import static java.util.Objects.isNull;
-import static java.util.Objects.nonNull;
 import static java.util.Optional.of;
+import static java.util.UUID.randomUUID;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static javax.ws.rs.core.Response.Status.ACCEPTED;
+import static org.apache.activemq.artemis.utils.RandomUtil.randomSimpleString;
 import static org.apache.commons.collections.CollectionUtils.isEmpty;
+import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -22,7 +24,6 @@ import static uk.gov.moj.cpp.courtscheduler.integration.utils.StubUtil.stubGetRe
 import static uk.gov.moj.cpp.courtscheduler.integration.utils.StubUtil.stubGetReferenceDataRotaBusinessTypes;
 
 import uk.gov.moj.cpp.courtscheduler.common.AzureBlobClientService;
-import uk.gov.moj.cpp.courtscheduler.integration.utils.FileUtil;
 import uk.gov.moj.cpp.courtscheduler.persist.entity.CourtSchedule;
 import uk.gov.moj.cpp.courtscheduler.persist.entity.CourtScheduleJudiciary;
 import uk.gov.moj.cpp.courtscheduler.persist.entity.CourtSchedulerMigrationStatus;
@@ -33,6 +34,7 @@ import java.io.InputStream;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.core.Response;
@@ -40,6 +42,7 @@ import javax.ws.rs.core.Response;
 import com.google.common.base.Stopwatch;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -63,8 +66,10 @@ class RotaFileProcessorIT extends AbstractIT {
     private LocalDateTime maxCreatedOnForCourtSchedule;
     private LocalDateTime maxUpdatedOnForCourtSchedule;
 
+    private static final List<String> filesToBeDeletedFromOutputContainer = new ArrayList<>();
+
     @BeforeAll
-    static void setupSystemUser() {
+    static void setupRotaFileProcessorIT() {
         setupUserAsSystemUser(USER_ID.toString());
         stubGetReferenceDataRotaBusinessTypes("referencedata.rota-business-types.json");
         stubGetReferenceCourtRooms("referencedata.rota-courtrooms.json");
@@ -75,6 +80,11 @@ class RotaFileProcessorIT extends AbstractIT {
     @BeforeEach
     public void setUpAzureBlobClientService() {
         setField(azureBlobClientService, "rotaslStorageConnectionString", rotaslStorageConnectionString);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        filesToBeDeletedFromOutputContainer.forEach(fileToBeDeleted -> azureBlobClientService.deleteFile(fileToBeDeleted, of(azureBlobOutputContainerName)));
     }
 
     @Test
@@ -96,9 +106,12 @@ class RotaFileProcessorIT extends AbstractIT {
 
         final Stopwatch stopwatch = Stopwatch.createStarted();
         final LocalDate snapshotFileStartDate = LocalDate.of(2024, 8, 1);
-        final String snapshotFileName = "IT_Test_lja_bedfodshire_snapshot_20240403T180039Z.xml";
-        azureBlobClientService.deleteFile(snapshotFileName, of(azureBlobOutputContainerName));
-        final InputStream rotaFileInputStream = FileUtil.class.getClassLoader().getResourceAsStream(format("rotafileprocessor/%s", snapshotFileName));
+        final String snapshotFileBaseNamePart1 = "IT_Test_lja_bedfodshire";
+        final String snapshotFileBaseNamePart2 = "_snapshot_20240403T180039Z";
+        final String generatedUniqueFileId = randomUUID().toString();
+        final String finalSnapshotFileName = format("%s_%s%s.xml", snapshotFileBaseNamePart1, generatedUniqueFileId, snapshotFileBaseNamePart2);
+
+        final InputStream rotaFileInputStream = getClass().getClassLoader().getResourceAsStream(format("rotafileprocessor/%s%s.xml", snapshotFileBaseNamePart1, snapshotFileBaseNamePart2));
 
         if (isNull(rotaFileInputStream)) {
             fail("rotaFileInputStream is null");
@@ -106,7 +119,7 @@ class RotaFileProcessorIT extends AbstractIT {
         }
         final byte[] rotaFileAsBytes = IOUtils.toByteArray(rotaFileInputStream);
         // upload the rota file first
-        azureBlobClientService.uploadProcessedFile(new ByteArrayInputStream(rotaFileAsBytes), (long) rotaFileAsBytes.length, snapshotFileName, of(azureBlobInputContainerName));
+        azureBlobClientService.uploadProcessedFile(new ByteArrayInputStream(rotaFileAsBytes), (long) rotaFileAsBytes.length, finalSnapshotFileName, of(azureBlobInputContainerName));
 
         final String payloadAsJsonString = getPayload("rota-file-processor-request.json");
         // then call rota file processor api
@@ -114,8 +127,8 @@ class RotaFileProcessorIT extends AbstractIT {
 
         // await until this file uploaded into archive container
         await().timeout(DEFAULT_POLL_TIMEOUT_FOR_ROTA_FILE_PROCESS_IN_SEC, SECONDS).until(() -> {
-            final byte[] downloadedBlobFromOutputContainer = azureBlobClientService.downloadFile(snapshotFileName, azureBlobOutputContainerName);
-            return nonNull(downloadedBlobFromOutputContainer) && downloadedBlobFromOutputContainer.length > 0;
+            final List<CourtSchedule> courtSchedulesFromSnapshotFile = databaseReader.courtSchedulesCreatedAfter(maxCreatedOnForCourtSchedule);
+            return isNotEmpty(courtSchedulesFromSnapshotFile);
         });
 
         logger.info("snapshot rota file processing took time as seconds : {}", stopwatch.elapsed(SECONDS));
@@ -132,14 +145,17 @@ class RotaFileProcessorIT extends AbstractIT {
         assertEquals(0, courtSchedulesFromSnapshotFile.stream()
                 .filter(courtSchedule -> courtSchedule.getSessionDate().isBefore(snapshotFileStartDate)).toList().size());
         assertEquals(45, courtScheduleJudiciaryEntities.size());
+
+        filesToBeDeletedFromOutputContainer.add(finalSnapshotFileName);
     }
 
     private void processFullRotaFile(final boolean migrated) throws SQLException, IOException {
         final Stopwatch stopwatch = Stopwatch.createStarted();
 
-        final String fileBlobName = "IT_Test_lja_bedfordshire_rota_20240402T180039Z.xml";
-        azureBlobClientService.deleteFile(fileBlobName, of(azureBlobOutputContainerName));
-        final InputStream rotaFileInputStream = FileUtil.class.getClassLoader().getResourceAsStream(format("rotafileprocessor/%s", fileBlobName));
+        final String fileBlobBaseName = "IT_Test_lja_bedfordshire_rota_20240402T180039Z";
+        final String generatedUniqueFileId = randomSimpleString().toString();
+        final String finalMasterRotaFileName = format("%s_%s.xml", fileBlobBaseName, generatedUniqueFileId);
+        final InputStream rotaFileInputStream = getClass().getClassLoader().getResourceAsStream(format("rotafileprocessor/%s.xml", fileBlobBaseName));
 
         if (isNull(rotaFileInputStream)) {
             fail("rotaFileInputStream is null");
@@ -147,7 +163,7 @@ class RotaFileProcessorIT extends AbstractIT {
         }
         final byte[] rotaFileAsBytes = IOUtils.toByteArray(rotaFileInputStream);
         // upload the rota file first
-        azureBlobClientService.uploadProcessedFile(new ByteArrayInputStream(rotaFileAsBytes), (long) rotaFileAsBytes.length, fileBlobName, of(azureBlobInputContainerName));
+        azureBlobClientService.uploadProcessedFile(new ByteArrayInputStream(rotaFileAsBytes), (long) rotaFileAsBytes.length, finalMasterRotaFileName, of(azureBlobInputContainerName));
         insertCourtSchedulerMigrationStatus(migrated);
 
         final String payloadAsJsonString = getPayload("rota-file-processor-request.json");
@@ -156,8 +172,8 @@ class RotaFileProcessorIT extends AbstractIT {
 
         // await until this file uploaded into archive container
         await().timeout(DEFAULT_POLL_TIMEOUT_FOR_ROTA_FILE_PROCESS_IN_SEC, SECONDS).until(() -> {
-            final byte[] downloadedBlobFromOutputContainer = azureBlobClientService.downloadFile(fileBlobName, azureBlobOutputContainerName);
-            return nonNull(downloadedBlobFromOutputContainer) && downloadedBlobFromOutputContainer.length > 0;
+            final List<CourtScheduleJudiciary> courtScheduleJudiciaryEntities = databaseReader.courtScheduleJudiciaries();
+            return isNotEmpty(courtScheduleJudiciaryEntities);
         });
 
         logger.info("master rota file processing took time as seconds : {}", stopwatch.elapsed(SECONDS));
@@ -169,7 +185,7 @@ class RotaFileProcessorIT extends AbstractIT {
         final List<CourtScheduleJudiciary> courtScheduleJudiciaryEntities = databaseReader.courtScheduleJudiciaries();
 
         if (!migrated) {
-            Pair<LocalDateTime, LocalDateTime> maxCreatedUpdatedPair = databaseReader.getMaxCreatedOnFromCourtSchedule();
+            final Pair<LocalDateTime, LocalDateTime> maxCreatedUpdatedPair = databaseReader.getMaxCreatedOnFromCourtSchedule();
             maxCreatedOnForCourtSchedule = maxCreatedUpdatedPair.getLeft();
             maxUpdatedOnForCourtSchedule = maxCreatedUpdatedPair.getRight();
             assertEquals(623, courtScheduleEntities.size());
@@ -180,6 +196,8 @@ class RotaFileProcessorIT extends AbstractIT {
             assertTrue(isEmpty(courtSchedulesUpdatedForMigrated));
         }
         assertEquals(45, courtScheduleJudiciaryEntities.size());
+
+        filesToBeDeletedFromOutputContainer.add(finalMasterRotaFileName);
     }
 
     private void insertCourtSchedulerMigrationStatus(final boolean migrated) throws SQLException {
