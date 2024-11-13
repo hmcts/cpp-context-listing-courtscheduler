84dd0f7cb8d1488bdd86ad7253b91298ca5d48a3
Ragubalan Durairaj <ragubalan.durairaj@hmcts.net>
2024-11-13 14:11:24 +0000

    DD-35686: Added distinct keyword to the query

--- listingcourtscheduler-viewstore/listingcourtscheduler-viewstore-persistence/src/main/java/uk/gov/moj/cpp/courtscheduler/repository/CourtScheduleRepository.java
+++ listingcourtscheduler-viewstore/listingcourtscheduler-viewstore-persistence/src/main/java/uk/gov/moj/cpp/courtscheduler/repository/CourtScheduleRepository.java
@@ -176,7 +176,7 @@ public abstract class CourtScheduleRepository extends AbstractEntityRepository<C
     }
 
     public CourtSchedule retrieveCourtScheduleWithListingById(final String courtScheduleId) {
-        StringBuilder queryString = new StringBuilder("SELECT s.*, case when al.id is not null then true else false end as hasHearingsBooked FROM court_schedule s left outer join  allocated_listings al on(s.id = al.court_schedule_id)  WHERE active = true AND s.id  = :courtScheduleId");
+        StringBuilder queryString = new StringBuilder("SELECT distinct s.*, case when al.id is not null then true else false end as hasHearingsBooked FROM court_schedule s left outer join  allocated_listings al on(s.id = al.court_schedule_id)  WHERE active = true AND s.id  = :courtScheduleId");
         final javax.persistence.Query query = entityManager.createNativeQuery(queryString.toString(), "CourtScheduleEntityMapping");
         query.setParameter("courtScheduleId", courtScheduleId);
         return (CourtSchedule) query.getSingleResult();
