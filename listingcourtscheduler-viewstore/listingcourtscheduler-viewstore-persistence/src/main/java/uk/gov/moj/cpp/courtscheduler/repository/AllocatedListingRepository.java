6e4864b07c656176a9b88296a09812bb551b2176
ArcadiusHMCTC <arcadius.ahouansou1@hmcts.net>
2025-02-17 11:07:11 +0000

    Team/cct 1981 (#312)
    * Tmp/from main 003 (#305)
    
    * listingcourtscheduler release 25.01 (#254)
    
    * team branch pom version update
    
    * DD-35058 creating endpoint to delete redundant rota data
    
    * DD-35058 fixing IT test failure-RotaFileProcessorIT-shouldCleanRedundantRotaData
    
    * disable rotafileprocessorit to see if federated credentials created
    
    * New 17.0.29-TECHWORK-1984-SNAPSHOT
    
    * enable RotaFileProcessorIT to connect via managed identity
    
    * New 17.0.30-TECHWORK-1984-SNAPSHOT
    
    * changing the pom version name to liase with branch name
    
    * New 17.0.31-TECHWORK-SNAPSHOT
    
    * DD-35624 dropping unused tables, most of being refdata tables
    
    * referencedata version update 17.0.33
    
    * New 17.0.32-TECHWORK-SNAPSHOT
    
    * update pom version to equalize with main branch to release
    
    * DD-36021 panel, courtsession multiple different values to be searched-get.hearing.slots endpoint
    
    * Merge branch 'team/rel2426' into dev/DD-36021
    
    * updating pom version to have branch naming
    
    * New 17.0.29-TECHWORKREL-SNAPSHOT
    
    * updated pom versions for release branch
    
    * New 17.0.32-SNAPSHOT
    
    ---------
    
    Co-authored-by: Mehmet Alpak <mehmet.alpak2@HMCTS.NET>
    Co-authored-by: samialpak <39771835+samialpak@users.noreply.github.com>
    Co-authored-by: devops-team <devops-team@hmcts.net>
    
    * updating poms for 17.0.33-SNAPSHOT development
    
    * updating poms for 17.0.33 branch with snapshot versions
    
    * updating poms for 17.0.34-SNAPSHOT development
    
    * updating poms for branch'dev/release-17.0.33' with non-snapshot versions
    
    * updating develop poms to master versions to avoid merge conflicts
    
    * Updating develop poms back to pre merge state
    
    * fixing IT test failures caused by hard coded startDate on create.courtscheduler request payload
    
    * updating poms for 17.0.34 branch with snapshot versions
    
    * updating poms for 17.0.35-SNAPSHOT development
    
    * updating poms for branch'dev/release-17.0.34' with non-snapshot versions
    
    * updating develop poms to master versions to avoid merge conflicts
    
    * Updating develop poms back to pre merge state
    
    * DD-37036 tactical fix for judiciary updates for rota-deletion of johs data even if allocated, inserting back again if the same/needed
    
    * team branch pom version update
    
    * New 17.0.36-DD-37036-SNAPSHOT
    
    * DD-37018: Rebased main and updated pom version to match main
    
    * New 17.0.36-SNAPSHOT
    
    * updating poms for 17.0.36 branch with snapshot versions
    
    * updating poms for 17.0.37-SNAPSHOT development
    
    * updating poms for branch'dev/release-17.0.36' with non-snapshot versions
    
    * updating develop poms to master versions to avoid merge conflicts
    
    * Updating develop poms back to pre merge state
    
    ---------
    
    Co-authored-by: Baris Ozturkmen <126248032+ozturkmenb@users.noreply.github.com>
    Co-authored-by: Mehmet Alpak <mehmet.alpak2@HMCTS.NET>
    Co-authored-by: samialpak <39771835+samialpak@users.noreply.github.com>
    Co-authored-by: devops-team <devops-team@hmcts.net>
    Co-authored-by: Ragubalan Durairaj <ragubalan.durairaj@hmcts.net>
    Co-authored-by: meetvipinp <61233682+meetvipinp@users.noreply.github.com>
    Co-authored-by: Arcadius Ahouansou <arcadius@menelic.com>
    
    * New 17.0.35-cct-1981-SNAPSHOT
    
    ---------
    
    Co-authored-by: Baris Ozturkmen <126248032+ozturkmenb@users.noreply.github.com>
    Co-authored-by: Mehmet Alpak <mehmet.alpak2@HMCTS.NET>
    Co-authored-by: samialpak <39771835+samialpak@users.noreply.github.com>
    Co-authored-by: devops-team <devops-team@hmcts.net>
    Co-authored-by: Ragubalan Durairaj <ragubalan.durairaj@hmcts.net>
    Co-authored-by: meetvipinp <61233682+meetvipinp@users.noreply.github.com>
    Co-authored-by: Arcadius Ahouansou <arcadius@menelic.com>

--- listingcourtscheduler-viewstore/listingcourtscheduler-viewstore-persistence/src/main/java/uk/gov/moj/cpp/courtscheduler/repository/AllocatedListingRepository.java
+++ listingcourtscheduler-viewstore/listingcourtscheduler-viewstore-persistence/src/main/java/uk/gov/moj/cpp/courtscheduler/repository/AllocatedListingRepository.java
@@ -31,7 +31,6 @@ import org.apache.deltaspike.data.api.Repository;
 public abstract class AllocatedListingRepository extends AbstractFullEntityRepository<AllocatedListing, String> {
 
     private static final String DELETE_REDUNDANT_ROTA_DATA = "DELETE FROM allocated_listings WHERE court_schedule_id IN (SELECT cs.id FROM court_schedule cs WHERE cs.session_start < (CURRENT_DATE - :numberOfDays))";
-
     @Inject
     private EntityManager entityManager;
 
