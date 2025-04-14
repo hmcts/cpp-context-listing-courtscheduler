6a379cd01c183618a0512f9abc7af01ee252d9d5
Baris Ozturkmen <baris.ozturkmen@hmcts.net>
2025-04-14 21:54:26 +0100

    removed unused file

--- listingcourtscheduler-integration-test/src/test/java/uk/gov/moj/cpp/courtscheduler/integration/BaseIntegrationTest.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.moj.cpp.courtscheduler.integration;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-
-@ExtendWith(TestDurationExtension.class)
-public abstract class BaseIntegrationTest extends AbstractIT {
-    // All integration tests will inherit the duration tracking functionality
-} 
