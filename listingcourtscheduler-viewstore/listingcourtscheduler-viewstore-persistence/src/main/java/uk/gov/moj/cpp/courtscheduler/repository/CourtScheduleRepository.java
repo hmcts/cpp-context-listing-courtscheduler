adffedf3579c13732573c866f21f06953d85259f
koraykaya <koray.kaya3@hmcts.net>
2025-04-22 13:23:59 +0100

    slotBased qeury fix

--- listingcourtscheduler-viewstore/listingcourtscheduler-viewstore-persistence/src/main/java/uk/gov/moj/cpp/courtscheduler/repository/CourtScheduleRepository.java
+++ listingcourtscheduler-viewstore/listingcourtscheduler-viewstore-persistence/src/main/java/uk/gov/moj/cpp/courtscheduler/repository/CourtScheduleRepository.java
@@ -654,8 +654,8 @@ public abstract class CourtScheduleRepository extends AbstractEntityRepository<C
         if (StringUtils.isNotBlank(requestParam.businessType())) {
             query.append(" AND cs.rota_business_type = :businessType");
         } else {
-            if (isNotEmpty(requestParam.isSlotBased()) && requestParam.isSlotBased()) {
-                query.append(" AND cs.is_slot_based = :slotBased");
+            if (isNotEmpty(requestParam.isSlotBased())) {
+                query.append(" AND cs.is_slot_based = ").append(requestParam.isSlotBased());
             }
         }
 
