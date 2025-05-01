b6b99e5e1d01bf364db124e6e7420950cacd59e9
Baris Ozturkmen <baris.ozturkmen@hmcts.net>
2025-05-01 10:58:00 +0100

    updated imports

--- listingcourtscheduler-viewstore/listingcourtscheduler-viewstore-persistence/src/test/java/uk/gov/moj/cpp/courtscheduler/repository/CourtScheduleRepositoryTest.java
+++ listingcourtscheduler-viewstore/listingcourtscheduler-viewstore-persistence/src/test/java/uk/gov/moj/cpp/courtscheduler/repository/CourtScheduleRepositoryTest.java
@@ -23,6 +23,7 @@ import uk.gov.moj.cpp.courtscheduler.domain.RequestedCourtSchedule;
 import uk.gov.moj.cpp.courtscheduler.domain.RequestedSlots;
 import uk.gov.moj.cpp.courtscheduler.domain.Result;
 import uk.gov.moj.cpp.courtscheduler.domain.utils.DateUtils;
+import uk.gov.moj.cpp.courtscheduler.domain.utils.TimezoneUtils;
 import uk.gov.moj.cpp.courtscheduler.persist.entity.AllocatedListing;
 import uk.gov.moj.cpp.courtscheduler.persist.entity.CourtSchedule;
 import uk.gov.moj.cpp.courtscheduler.persist.entity.CourtScheduleJudiciary;
