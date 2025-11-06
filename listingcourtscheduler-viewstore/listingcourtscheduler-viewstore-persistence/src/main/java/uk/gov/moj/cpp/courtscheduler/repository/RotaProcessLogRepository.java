f3708f16b0b60126e8c95dbaf2b5ea17c99ea184
koraykaya <koray.kaya3@hmcts.net>
2025-11-06 10:41:57 +0000

    id column fix

--- listingcourtscheduler-viewstore/listingcourtscheduler-viewstore-persistence/src/main/java/uk/gov/moj/cpp/courtscheduler/repository/RotaProcessLogRepository.java
+++ listingcourtscheduler-viewstore/listingcourtscheduler-viewstore-persistence/src/main/java/uk/gov/moj/cpp/courtscheduler/repository/RotaProcessLogRepository.java
@@ -2,11 +2,13 @@ package uk.gov.moj.cpp.courtscheduler.repository;
 
 import uk.gov.moj.cpp.courtscheduler.persist.entity.RotaProcessLog;
 
+import java.util.UUID;
+
 import org.apache.deltaspike.data.api.AbstractEntityRepository;
 import org.apache.deltaspike.data.api.Repository;
 
 @Repository(forEntity = RotaProcessLog.class)
-public abstract class RotaProcessLogRepository extends AbstractEntityRepository<RotaProcessLog, String> {
+public abstract class RotaProcessLogRepository extends AbstractEntityRepository<RotaProcessLog, UUID> {
 
     private static final String DELETE_REDUNDANT_ROTA_DATA = "DELETE FROM rota_process_log WHERE timestamp < (CURRENT_DATE - :numberOfDays)";
 
