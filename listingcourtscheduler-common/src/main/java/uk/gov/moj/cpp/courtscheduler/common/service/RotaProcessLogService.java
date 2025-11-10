2d34d67631c13350c789e4047a39bc7f86aed24d
Baris Ozturkmen <baris.ozturkmen@hmcts.net>
2025-11-10 02:13:07 +0000

    updated transaction type for rotaprocess log

--- listingcourtscheduler-common/src/main/java/uk/gov/moj/cpp/courtscheduler/common/service/RotaProcessLogService.java
+++ listingcourtscheduler-common/src/main/java/uk/gov/moj/cpp/courtscheduler/common/service/RotaProcessLogService.java
@@ -2,12 +2,14 @@ package uk.gov.moj.cpp.courtscheduler.common.service;
 
 import static javax.transaction.Transactional.TxType.REQUIRES_NEW;
 
-import uk.gov.moj.cpp.courtscheduler.persist.entity.RotaProcessLog;
-import uk.gov.moj.cpp.courtscheduler.repository.RotaProcessLogRepository;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+
+import uk.gov.moj.cpp.courtscheduler.persist.entity.RotaProcessLog;
+import uk.gov.moj.cpp.courtscheduler.repository.RotaProcessLogRepository;
 
 @ApplicationScoped
 public class RotaProcessLogService {
@@ -15,12 +17,17 @@ public class RotaProcessLogService {
     @Inject
     private RotaProcessLogRepository rotaProcessLogRepository;
 
+    @PersistenceContext(unitName = "courtscheduler-persistence-unit")
+    private EntityManager entityManager;
+
     @Transactional(REQUIRES_NEW)
     public RotaProcessLog saveRotaProcessLog(final RotaProcessLog rotaProcessLog) {
-        return rotaProcessLogRepository.save(rotaProcessLog);
+        this.entityManager.persist(rotaProcessLog);
+        this.entityManager.flush();
+        return rotaProcessLog;
     }
 
     public int deleteRedundantRotaData(final int numberOfPreviousMonths) {
-        return rotaProcessLogRepository.deleteRedundantRotaData(numberOfPreviousMonths * 30);
+        return this.rotaProcessLogRepository.deleteRedundantRotaData(numberOfPreviousMonths * 30);
     }
 }
