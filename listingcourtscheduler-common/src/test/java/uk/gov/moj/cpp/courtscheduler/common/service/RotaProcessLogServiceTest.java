4f83f3798cc5532d77a3c25a342abdea242660ce
Baris Ozturkmen <baris.ozturkmen@hmcts.net>
2025-11-10 02:44:18 +0000

    updated mock

--- listingcourtscheduler-common/src/test/java/uk/gov/moj/cpp/courtscheduler/common/service/RotaProcessLogServiceTest.java
+++ listingcourtscheduler-common/src/test/java/uk/gov/moj/cpp/courtscheduler/common/service/RotaProcessLogServiceTest.java
@@ -11,6 +11,8 @@ import static org.mockito.Mockito.when;
 import uk.gov.moj.cpp.courtscheduler.persist.entity.RotaProcessLog;
 import uk.gov.moj.cpp.courtscheduler.repository.RotaProcessLogRepository;
 
+import javax.persistence.EntityManager;
+
 import io.github.benas.randombeans.api.EnhancedRandom;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,11 +29,12 @@ class RotaProcessLogServiceTest {
     @Mock
     private RotaProcessLogRepository rotaProcessLogRepository;
 
+    @Mock
+    private EntityManager entityManager;
+
     @Test
     void shouldSaveRotaProcessLog() {
         final RotaProcessLog rotaProcessLog = EnhancedRandom.random(RotaProcessLog.class);
-
-        when(rotaProcessLogRepository.save(rotaProcessLog)).thenReturn(rotaProcessLog);
 
         final RotaProcessLog rotaProcessLogSaved = rotaProcessLogService.saveRotaProcessLog(rotaProcessLog);
 
